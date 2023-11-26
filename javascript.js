ce326c8ae2f13588fb77e3d4e9559bd2af2b7a6f
steveAzo -Git <stephenazongo77@gmail.com>
2023-11-26 00:00:20 +0000

    Did some minor changes

--- javascript.js
+++ javascript.js
@@ -1,11 +1,8 @@
-// include.js
 document.addEventListener("DOMContentLoaded", function () {
-    // Include header
     var headerInclude = document.createElement("header");
     headerInclude.innerHTML = '<object type="text/html" data="header.html"></object>';
     document.body.prepend(headerInclude);
     
-    // Include footer
     var footerInclude = document.createElement("footer");
     footerInclude.innerHTML = '<object type="text/html" data="footer.html"></object>';
     document.body.appendChild(footerInclude);
